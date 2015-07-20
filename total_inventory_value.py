cf3c6e6039cc98ca2015af5d3d3d5a848d8f8ce1
Prachi Shukla <shuklaprachi21@gmail.com>
2015-07-20 16:17:35 -0700

    calculating total inventory value in a store

---
+++ total_inventory_value.py
@@ -0,0 +1,26 @@
+#Owning a store ; calculating total value of inventory
+prices = {
+    "banana" : 4,
+    "apple"  : 2,
+    "orange" : 1.5,
+    "pear"   : 3,
+}
+stock = {
+    "banana" : 6,
+    "apple"  : 0,
+    "orange" : 32,
+    "pear"   : 15,
+}
+
+for key in prices:
+    print key
+    print "price: %s" % prices[key]
+    print "stock: %s" % stock[key]
+    
+total = 0
+for key in prices:
+    cost = prices[key] * stock[key]
+    print cost
+    total = total + cost
+
+print total
