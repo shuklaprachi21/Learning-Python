8244d1cf3129bba641938654e70e0461c4be7939
Prachi Shukla <shuklaprachi21@gmail.com>
2015-07-20 12:43:46 -0700

    list slicing

---
+++ list_slicing.py
@@ -0,0 +1,7 @@
+#NOTE: We start at the index before the colon and continue up to but not including the index after the colon.
+
+suitcase = ["sunglasses", "hat", "passport", "laptop", "suit", "shoes"]
+
+first  = suitcase[0:2]  # The first and second items (index zero and one)
+middle = suitcase[2:4]  # Third and fourth items (index two and three)
+last   = suitcase[4:6]  # The last two items (index four and five)
