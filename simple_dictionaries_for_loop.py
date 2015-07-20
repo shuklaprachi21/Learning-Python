c09a73569fcac28e265bc89a023686188a856161
Prachi Shukla <shuklaprachi21@gmail.com>
2015-07-20 14:57:06 -0700

    simple for-loop iteration over a dictionary

---
+++ simple_dictionaries_for_loop.py
@@ -0,0 +1,11 @@
+#Dictionaries and for-loop
+webster = {
+	"Aardvark" : "A star of a popular children's cartoon show.",
+    "Baa" : "The sound a goat makes.",
+    "Carpet": "Goes on the floor.",
+    "Dab": "A small amount."
+}
+
+
+for key in webster:
+    print webster[key]
