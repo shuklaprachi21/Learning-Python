5ddee34a006d44c422d28161157f28d37b3473c9
Prachi Shukla <shuklaprachi21@gmail.com>
2015-07-20 12:35:44 -0700

    simple list append() and len() functions

---
+++ list_append_len.py
@@ -0,0 +1,14 @@
+suitcase = [] 
+suitcase.append("sunglasses")
+
+# Your code here!
+suitcase.append("bathing suit")
+suitcase.append("sunblock")
+suitcase.append("hat")
+
+
+
+list_length = len(suitcase) # Set this to the length of suitcase
+
+print "There are %d items in the suitcase." % (list_length)
+print suitcase
