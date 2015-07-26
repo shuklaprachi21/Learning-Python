531805bf7de4123b03ce1be4d8d81b009dfbac40
Prachi Shukla <shuklaprachi21@gmail.com>
2015-07-26 11:53:23 -0700

    introduction to inheritance

---
+++ inheritance.py
@@ -0,0 +1,18 @@
+class Employee(object):
+    def __init__(self, employee_name):
+        self.employee_name = employee_name
+
+    def calculate_wage(self, hours):
+        self.hours = hours
+        return hours * 20.00
+
+class PartTimeEmployee(Employee):
+    def calculate_wage(self, hours):
+        self.hours = hours;
+        return hours * 12.00
+        
+    def full_time_wage(self, hours):
+        return super(PartTimeEmployee, self).calculate_wage(hours)
+        
+milton = PartTimeEmployee("John")
+print milton.full_time_wage(10)
